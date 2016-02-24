e7245c95a6165da532519d2d78479725134e0926
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-02-24 20:58:11 +0100

    akutalizacja

--- wszystko/testy2/wyrazenia.php
+++ wszystko/testy2/wyrazenia.php
@@ -7,6 +7,25 @@
  */
 
 
+//if (preg_match('@^[0-9]{2}-[0-9]{3}$@', $_POST['pole'])) {
+//    echo('ok');
+//} else {
+//    echo("error");
+//}
+
+
+//if (preg_match('@^[A-Z][a-z]{2,10} [A-Z][a-z]{2,15}$@', $_POST['pole'])) {
+//    echo('ok');
+//} else {
+//    echo("error");
+//}
+
+
+if (preg_match('@^[^a-z]+$@', $_POST['pole'])) {
+    echo('ok');
+} else {
+    echo("error");
+}
 ?>
 
 <form method="post" action="wyrazenia.php">
