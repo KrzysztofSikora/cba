4a067576227f184d2b705c0382ffd0a8bbd39167
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-02-24 20:10:13 +0100

    Initial commit

---
+++ wszystko/testy2/wyrazenia.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krzysztof
+ * Date: 24.02.16
+ * Time: 19:59
+ */
+
+
+?>
+
+<form method="post" action="wyrazenia.php">
+    <input type="text" name="pole">
+    <input type="submit" value="Wyslij">
+</form>
