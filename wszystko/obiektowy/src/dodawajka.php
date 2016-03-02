89e582c706bd6e3d985b412943ba6e33c3a2bee4
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-03-02 10:58:31 +0100

    akutalizacja

--- wszystko/obiektowy/src/dodawajka.php
+++ wszystko/obiektowy/src/dodawajka.php
@@ -6,9 +6,18 @@
  * Time: 19:28
  */
 
+$db = mysqli_connect("krzysztofsikora24.pl","krzysztofcba","bazacba12","krzysztofsikora24_pl"); //keep your db name
+$image = addslashes(file_get_contents($_FILES['images']['tmp_name']));
+//you keep your column name setting for insertion. I keep image type Blob.
+$query = "INSERT INTO products (productID,image) VALUES('','$image')";
+$qry = mysqli_query($db, $query);
+
+
+
 ?>
 
-<FORM ACTION="upload.php" METHOD="POST" ENCTYPE="multipart/form-data">
-    Zdjęcie: </td><td><INPUT type="file" name="zdjecie">
+<FORM ACTION="dodawajka.php" METHOD="POST" ENCTYPE="multipart/form-data">
+    Zdjęcie: </td><td><INPUT type="file" name="images">
 <input type="submit" name="ok" value="Wyślij zdjęcie do bazy"/>
 </FORM>
+
--- wszystko/obiektowy/src/showimage.php
+++ wszystko/obiektowy/src/showimage.php
@@ -6,22 +6,12 @@
  * Time: 19:36
  */
 
-header("Content-type: image/jpg;");
 
-$uzytkownik = "krzysztofcba";         //
-$haslo = "bazacba12";                // Rzecz jasna wszystkie te dane zależą od naszej konkretnej bazy!
-$db_name = "krzysztofsikora24_pl";          //
-$adres = "krzysztofsikora24.pl";         //        //
 
-$link = mysql_connect( $adres, $uzytkownik, $haslo);
-mysql_select_db($db);
-$id = $_GET['id'];
-$result = mysql_query("SELECT zdjecie FROM zdjecia WHERE id= '$id'");
-if (mysql_num_rows($result) != 0)
-{
-    $row = mysql_fetch_assoc($result);
-    echo base64_decode($row['zdjecie']);
-    echo ($row['id']);
-}
+$db = mysqli_connect("krzysztofsikora24.pl","krzysztofcba","bazacba12","krzysztofsikora24_pl"); //keep your db name
+$sql = "SELECT * FROM products WHERE productID = 40";
+$sth = $db->query($sql);
+$result=mysqli_fetch_array($sth);
+echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['image'] ).'"/>';
 
 ?>
