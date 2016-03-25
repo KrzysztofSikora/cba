2c4e784ae29d3c4681df6d61ed3dc968472518ed
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-03-26 00:05:08 +0100

    I've configured Symfony2 routing.

---
+++ sym/src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krzysztof
+ * Date: 25.03.16
+ * Time: 23:08
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName) {
+        return new Response('The genus genusname:'. $genusName);
+    }
+}
