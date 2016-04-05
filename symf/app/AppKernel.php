511c687088297a131c73190ab5228fe988eec8e8
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-04-05 21:43:40 +0200

    Symfony2 routes and templates are working. Bundle add.

--- symf/app/AppKernel.php
+++ symf/app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Login\LoginBundle\LoginLoginBundle(),
-            new LoginLoginBundle\LoginLoginBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
